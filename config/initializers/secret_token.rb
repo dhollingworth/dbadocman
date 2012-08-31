094abb585ed497b0f4f7d715f68a0817d3ad9450
David Hollingworth <david@dba.ie>
2012-08-31 16:34:46 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dbadocman::Application.config.secret_token = '1e7df82d2eadd084f285f3380c387ce36812a15218ff0791246399c7309651573a9733749aeee07cbfdb99ff01ce7a491a0143cdc77daa0d8e4f53510bcfb7f6'
